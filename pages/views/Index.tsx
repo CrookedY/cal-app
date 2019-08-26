1cf6cee0e94f67fc592a2f65f81debcd75ab316e
Hannah Sewell <hannah_sewell1992@hotmail.com>
2019-08-26 17:21:50 +0100

    Custom routes are real easy apparently

--- pages/views/Index.tsx
+++ pages/views/Index.tsx
@@ -7,7 +7,9 @@ interface Props {
 
 const Index: NextPage<Props> = ({ query }) => {
   const greetName = query.name ? query.name : 'World';
-  return <div>Hello, {greetName}!</div>;
+  return <div>
+    <h1>Hello, {greetName}!</h1>
+    </div>;
 };
 
 Index.getInitialProps = async (ctx: NextPageContext) => {
--- src/application.module.ts
+++ src/application.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RenderModule } from 'nest-next';
 import { AppController } from './app.controller';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [RenderModule],
-  controllers: [AppController]
+  controllers: [AppController, CatsController]
 })
 export class AppModule {}
